a9389194fd3f6b50309c49eb468e596355439683
amandaacilene <amanda.cilene@hotmail.com>
2018-08-18 17:21:45 -0300

    create basic functions for using localstorage instead of mongo

--- src/util/utilActions.js
+++ src/util/utilActions.js
@@ -47,3 +47,43 @@ export const remove = (util) => {
 export const clear = () => {
     return [{ type: 'UTIL_CLEAR' }, search()]
 }
+
+const generateUUID = () => {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+    });
+}
+
+const getAllFromStorage = () => {
+    return JSON.parse(localStorage.getItem('tasks') || '[]');
+}
+
+const updateAllInStorage = (tasks) => {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+const addToStorage = (description) => {
+    const oldTasks = getAllFromStorage();
+    const task = { description, _id: generateUUID(), done: false };
+    const newTasks = [...oldTasks, task];
+    updateAllInStorage(newTasks);
+}
+
+const markAsDoneInStorage = (id) => {
+    const oldTasks = getAllFromStorage();
+    const newTasks = oldTasks.reduce((tasks, task) => tasks.push(task._id == id ? { ...task, done: true } : task) && tasks, []);
+    updateAllInStorage(newTasks);
+}
+
+const markAsPendingInStorage = (id) => {
+    const oldTasks = getAllFromStorage();
+    const newTasks = oldTasks.reduce((tasks, task) => tasks.push(task._id == id ? { ...task, done: false } : task) && tasks, []);
+    updateAllInStorage(newTasks);
+}
+
+const removeInStorage = (id) => {
+    const oldTasks = getAllFromStorage();
+    const newTasks = oldTasks.filter(task => task._id != id);
+    updateAllInStorage(newTasks);
+}
