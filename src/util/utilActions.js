9cb790e7544bbba66ee0d086175a19f2865f8426
Amanda C F de Azevedo <amandah.cilene@gmail.com>
2018-08-18 19:40:01 -0300

    create basic functions for using localstorage instead of mongo (#1)
    * create basic functions for using localstorage instead of mongo
    
    * add getmatched function
    
    * modify current actions, introducing them to localstorage usage

--- src/util/utilActions.js
+++ src/util/utilActions.js
@@ -9,41 +9,89 @@ export const changeDescription = event => ({
 
 export const search = () => {
     return (dispatch, getState) => {
-        const description = getState().util.description
-        const search = description ? `&description__regex=/${description}/` : '' 
-        const request = axios.get(`${URL}?sort=-createdAt${search}`)
-            .then(resp => dispatch({type: 'UTIL_SEARCHED', payload: resp.data})) 
+        const description = getState().util.description;
+        if (description) {
+            dispatch({type: 'UTIL_SEARCHED', payload: getMatched(description) });
+        } else {
+            dispatch({type: 'UTIL_SEARCHED', payload: getAllFromStorage() });
+        }
     }
 }
 
 export const add = (description) => {
     return dispatch => {
-        axios.post(URL, { description })
-            .then(resp => dispatch(clear()))
-            .then(resp => dispatch(search()))
-        }
+        addToStorage(description);
+        dispatch(clear());
+        dispatch(search());
+    }
 }
 
 export const markAsDone = (util) => { 
     return dispatch => {
-        axios.put(`${URL}/${util._id}`, { ...util, done: true }) 
-            .then(resp => dispatch(search()))
+        markAsDoneInStorage(util._id);
+        dispatch(search());
     } 
 }
 export const markAsPending = (util) => { 
     return dispatch => {
-        axios.put(`${URL}/${util._id}`, { ...util, done: false }) 
-            .then(resp => dispatch(search()))
+        markAsPendingInStorage(util._id);
+        dispatch(search());
     } 
 }
 
 export const remove = (util) => {
     return dispatch => {
-        axios.delete(`${URL}/${util._id}`)
-            .then(resp => dispatch(search()))
+        removeInStorage(util._id);
+        dispatch(search());
     }
 }
 
 export const clear = () => {
     return [{ type: 'UTIL_CLEAR' }, search()]
+}
+
+const generateUUID = () => {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+    });
+}
+
+const getAllFromStorage = () => {
+    return JSON.parse(localStorage.getItem('tasks') || '[]');
+}
+
+const updateAllInStorage = (tasks) => {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+const getMatched = (pattern) => {
+    const tasks = getAllFromStorage();
+    const foundTasks = tasks.filter(task => task.description.toLowerCase().indexOf(pattern.toLowerCase()) > -1);
+    return foundTasks;
+}
+
+const addToStorage = (description) => {
+    const oldTasks = getAllFromStorage();
+    const task = { description, _id: generateUUID(), done: false };
+    const newTasks = [...oldTasks, task];
+    updateAllInStorage(newTasks);
+}
+
+const markAsDoneInStorage = (id) => {
+    const oldTasks = getAllFromStorage();
+    const newTasks = oldTasks.reduce((tasks, task) => tasks.push(task._id == id ? { ...task, done: true } : task) && tasks, []);
+    updateAllInStorage(newTasks);
+}
+
+const markAsPendingInStorage = (id) => {
+    const oldTasks = getAllFromStorage();
+    const newTasks = oldTasks.reduce((tasks, task) => tasks.push(task._id == id ? { ...task, done: false } : task) && tasks, []);
+    updateAllInStorage(newTasks);
+}
+
+const removeInStorage = (id) => {
+    const oldTasks = getAllFromStorage();
+    const newTasks = oldTasks.filter(task => task._id != id);
+    updateAllInStorage(newTasks);
 }
